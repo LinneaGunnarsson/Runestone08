ec6046f6c024a484e7b5b9f2403ca13163087f9e
emisto <emil1stolpe@gmail.com>
2019-05-01 23:44:14 +0200

    can connect to server with IP, nameswaping button, sid

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
+button = null
+
 @socketio.on('message')
 def handleMessage(msg):
+	print('session id: ' + request.sid)
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
 
+@socketio.on('manual request')
+def handleManualReq():
+	send('take control', broadcast=True)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 if __name__ == '__main__':
-	socketio.run(app)
+	socketio.run(app, host = "192.168.1.106")
