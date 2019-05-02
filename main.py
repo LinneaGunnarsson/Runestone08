442773d8f8eae00167294943d42337d4e3ef7103
LinneaGunnarsson <linnea.gunnarsson@live.com>
2019-05-02 14:57:36 +0200

    added buttons

--- main.py
+++ main.py
@@ -4,19 +4,47 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
+button = 0
+
+def talkToRobot(command):
+	if (command == 'Up'):
+		send('up pressed', broadcast=True)
+	if (command == 'down'):
+		send('down pressed', broadcast=True)
+	if (command == 'right'):
+		send('right pressed', bradcast=True)
+	if (command == 'left'):
+		send('left pressed', broadcast=True)
+
+
 
 @socketio.on('message')
 def handleMessage(msg):
 	print('session id: ' + request.sid)
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
+	talkToRobot(msg)
 
 @socketio.on('manual request')
 def handleManualReq():
-	send('take control', broadcast=True)
+	global button
+	if(button == 0):
+		msg = " take control"
+		button = request.sid
+		send(msg, broadcast=True)
+   
+	elif (request.sid != button):
+		send("Ocupied")
+   
+	else:
+		send("released")
+		button = 0
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 if __name__ == '__main__':
-	socketio.run(app, host = "130.243.223.23")
+	socketio.run(app, host = "130.243.228.9")
+
+
